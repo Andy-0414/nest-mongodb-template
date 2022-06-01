edd1f5401311c2a5f4245d881bc7ed5993232e45
andy-0414 <pjh8667@naver.com>
2022-06-01 14:52:28 +0900

    user-manager/users 모듈 없이 서비스 추가

--- src/user-management/user-management.module.ts
+++ src/user-management/user-management.module.ts
@@ -1,10 +1,13 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UserManagementService } from './user-management.service';
 import { UserManagementController } from './user-management.controller';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
-	providers: [UserManagementService],
-	controllers: [UserManagementController],
+	providers: [UserManagementService, UsersService],
+	controllers: [UserManagementController, UsersController],
+	imports: [],
 })
 export class UserManagementModule {
 	configure(consumer: MiddlewareConsumer) {
